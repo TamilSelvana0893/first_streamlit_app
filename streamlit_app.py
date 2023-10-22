7df3c575779021ea98c6d7ac7bd03b222990398c
TamilSelvana0893 <148465561+TamilSelvana0893@users.noreply.github.com>
2023-10-22 11:53:45 +0300

    Update streamlit_app.py
    Added Emojis and Avocado Toast

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,8 @@ import streamlit
 streamlit.title('My Parents new Healthy Dinner')
 
 streamlit.header('Breakfast Favorites')
-streamlit.text('Omega 3 and Oatmeal')
-streamlit.text('Kale,Spinch & Rocket smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega 3 and Oatmeal')
+streamlit.text('🥗 Kale,Spinch & Rocket smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 
