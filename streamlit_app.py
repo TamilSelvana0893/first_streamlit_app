bc0428baebbdde91ae207488b0b1f68bdfa54574
TamilSelvana0893 <148465561+TamilSelvana0893@users.noreply.github.com>
2023-10-22 11:41:58 +0300

    Update streamlit_app.py
    Fixed error

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents new Healthy Dinner')
-print('Breakfast Menu')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and Oatmeal')
+streamlit.text('Kale,Spinch & Rocket smoothie)
+streamlit.text('Hard-Boiled Free-Range Egg')
+
