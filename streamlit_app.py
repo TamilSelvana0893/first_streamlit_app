d8f6bc6e6f149334b19d39b210228774da288c15
TamilSelvana0893 <148465561+TamilSelvana0893@users.noreply.github.com>
2023-10-22 11:42:41 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ streamlit.title('My Parents new Healthy Dinner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 and Oatmeal')
-streamlit.text('Kale,Spinch & Rocket smoothie)
+streamlit.text('Kale,Spinch & Rocket smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
