4bfd6efbbd8859afa26a6148c7eef92088bd814e
TamilSelvan Anbarasu <148465561+TamilSelvana0893@users.noreply.github.com>
2023-10-24 11:49:29 +0300

    Update streamlit_app.py
    Added IF else statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,16 +28,21 @@ streamlit.dataframe(fruits_to_show)
 
 #New selection to display frutyvice api response
 streamlit.header('Frutyvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+   fruit_choice = streamlit.text_input('What fruit would you like information about?')
+   if not fruit_choice:
+          Streamlit.error("Please select a fruit to get the information")
+   else:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+streamlit.error()
 
 #import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
-# take the JSON version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# Output it the screen as a table
-streamlit.dataframe(fruityvice_normalized)
+
+
 # Don't run anything past here while we trubleshoot
 streamlit.stop()
 #import snowflake.connector
